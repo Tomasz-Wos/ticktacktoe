16502a01eae14eac1722231d8e1ebaff01212c7c
Tomasz <t.wos93@gmail.com>
2024-04-07 13:56:02 +0200

    TickTackToe App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './components/square/square.component';
+import { BoardComponent } from './components/board/board.component';
+import { NbThemeModule} from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule
   ],
   providers: [
     provideClientHydration()
